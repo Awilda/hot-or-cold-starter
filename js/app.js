4cae712e9fa6d89e10f983a6a259306d8548dd26
Awilda <awildacantres@gmail.com>
2015-05-05 19:53:34 -0400

    Update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-
-$(document).ready(function(){
+$(document).ready(function() {
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -12,6 +11,59 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-});
+var guess;
+var target;
+var range=0;
 
+target = Math.floor(Math.random()*100);
+
+	$('#guessButton').click(function() {
+
+var noGuess = parseInt($('#count').text());
+$('#count').text(noGuess + 1);
+
+	guess = $("#userGuess").val();
+
+	if (guess > target) {
+   	range = guess - target;
+	}
+	else if (target > guess) {
+   	range = target - guess;
+	}
+	else if (guess = target){
+	alert("You're Right!");
+	return;
+	}
+
+	if(range<5){
+		$("#feedback").text("Burn!!!!");
+	}
+	else if(range>=5 && range<=10){
+		$("#feedback").text("Hot Hot!");
+	}
+	else if(range>10 && range<=20){
+		$("#feedback").text("Hot");
+	}
+	else if(range>20 && range<=30){
+		$("#feedback").text("Warm");
+	}
+	else if(range>30 && range<=40){
+		$("#feedback").text("Cold");
+	}
+	else if(range>40 && range<=50){
+		$("#feedback").text("Too Cold");
+	}
+	else if (range >50) {
+	}
+	else if (guess = target) {
+		$("#feedback").text("You're Right!");	
+	}
+
+	event.preventDefault();
+
+	$("ul#guessList").append("<li class='guessBox'>" + userGuess.value + "</li>");
+
+		
+	});
+});
 
